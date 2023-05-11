d96359eb9d314522f03ecc718d77757bc85f9e9a
BrockHanni <hannibr26@gmail.com>
2023-05-10 22:27:25 -0600

    Working Generator

---
+++ Develop/README.md
@@ -0,0 +1,38 @@
+# README Generator
+
+This is a README Generator, once you run the file in node, it will ask you a series of questions, that will automatically input themselves into a quality readme.md file.
+
+## Installation
+
+To use the README Generator, all you need to do is open the file 'index.js' install node by entering the command 'npm i' in your terminal, then running the file in the terminal 'node index.js'
+
+## Usage
+
+After you run node index.js, the terminal will ask you questions, simply type your answers based on your project, and you'll have a fully developed readme file.
+
+## Tests
+
+To run tests, run node index.js, and answer the questions, then check the readme file that is generated.
+
+## Contributing/Contact
+
+To submit bug reports, feature requests, or pull requests, please email me at Hannibr26@gmail.com, or visit my GitHub page: BrockHanni(https://github.com/BrockHanni)
+
+## License
+
+Licenses used: N/A
+
+## Deployment
+
+TODO
+
+## Table of Contents
+
+List out the sections in your README file, and provide links to each section. This makes it easy for readers to find the information they're looking for. Some common sections to include are:
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Tests](#tests)
+- [Contributing/Contact](#contributing/Contact)
+- [License](#license)
+- [Deployment](#deployment)
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,99 @@
-// TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+inquirer
 
-// TODO: Create an array of questions for user input
-const questions = [];
+    .prompt([
+        {
+          type: 'input',
+          message: 'Enter the title',
+          name: 'Title',
+        },
+        {
+          type: 'input',
+          message: 'Description:',
+          name: 'Description',
+        },
+        {
+            type: 'input',
+            message: 'Installation Instructions:',
+            name: 'Install',
+          },
+          {
+            type: 'input',
+            message: 'Usage Instructions:',
+            name: 'Usage',
+          },
+          {
+            type: 'input',
+            message: 'How to test your project:',
+            name: 'Test',
+          },
+          {
+            type: 'input',
+            message: 'Licenses used:',
+            name: 'license',
+          },
+          {
+            type: 'input',
+            message: 'Insert your GitHub URL',
+            name: 'GitHub',
+          },
+          {
+            type: 'input',
+            message: 'Github Username:',
+            name: 'GitHubUser',
+          },
+          {
+            type: 'input',
+            message: 'Email Address:',
+            name: 'Email',
+          },
+      ])
+      .then((answers) => {
+        // Write the answers to a text file
+        const data = 
+`# ${answers.Title}
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+${answers.Description}
 
-// TODO: Create a function to initialize app
-function init() {}
+## Installation
 
-// Function call to initialize app
-init();
+${answers.Install}
+
+## Usage
+
+${answers.Usage}
+
+## Test
+
+${answers.Test}
+
+## Contributing/Contact
+
+To submit bug reports, feature requests, or pull requests, please email me at ${answers.Email}, or visit my GitHub page: ${answers.GitHubUser}(https://github.com/${answers.GitHubUser})
+
+## License
+
+Licenses used: ${answers.license}
+
+## Deployment
+
+${answers.GitHub}
+
+## Table of Contents
+
+List out the sections in your README file, and provide links to each section. This makes it easy for readers to find the information they're looking for. Some common sections to include are:
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Test](#test)
+- [Contributing/Contact](#contributing/Contact)
+- [License](#license)
+- [Deployment](#deployment)`
+;
+    
+  fs.appendFile('README.md', data, (err) => {
+    if (err) throw err;
+    console.log('Answers saved to README.md');
+  });
+});
